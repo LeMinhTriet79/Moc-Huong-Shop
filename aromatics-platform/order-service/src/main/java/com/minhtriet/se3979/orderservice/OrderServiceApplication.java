42b3a11f74f3936536631298538cd119401855e1
LeMinhTriet79 <trietleminh7979@gmail.com>
2026-06-17 12:46:44 +0700

    khởi tạo

---
+++ aromatics-platform/order-service/src/main/java/com/minhtriet/se3979/orderservice/OrderServiceApplication.java
@@ -0,0 +1,13 @@
+package com.minhtriet.se3979.orderservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrderServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OrderServiceApplication.class, args);
+    }
+
+}
